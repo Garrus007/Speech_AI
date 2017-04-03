b1891ae9d04463adfed56b4a3a817af5fdf61905
Markov Alex <MarkovAlex95@gmail.com>
2017-04-03 20:30:13 +0300

    Заменил pygame, на pyadiuo, потому что pygame видимо не закрывает файл на Windows и нельзя воспроизвести больше одной фразы

--- speech_ai.py
+++ speech_ai.py
@@ -3,9 +3,7 @@ import speech_recognition as sr
 from gtts import gTTS
 
 # Воспроизведение речи
-import pygame
-from pygame import mixer
-mixer.init()
+import pyglet
 
 import os
 import time
@@ -50,16 +48,17 @@ class Speech_AI:
                     answer = self.make_answer(statement)
                     # Союда можно добавить много интересностей (IoT, CV, ...)
 
-                    self.say(str(answer))
-                    while pygame.mixer.music.get_busy():
-                        time.sleep(0.1)
-
                     print("Вы сказали: {}".format(statement))
                     print("{} сказал: {}".format(self.bot.name, answer))
+
+                    self.say(str(answer))
+
                 except sr.UnknownValueError:
                     print("Упс! Кажется, я тебя не понял")
+
                 except sr.RequestError as e:
                     print("Не могу получить данные от сервиса Google Speech Recognition; {0}".format(e))
+
         except KeyboardInterrupt:
             # Сохраняем данные для следующей сессии
             self.bot.trainer.export_for_training('corpus/last_session_corpus.json')
@@ -69,10 +68,19 @@ class Speech_AI:
     def say(self, phrase):
         tts = gTTS(text=phrase, lang="ru")
         tts.save(self._mp3_name)
+        self.playMp3(self._mp3_name)
+        
 
-        # Play answer
-        mixer.music.load(self._mp3_name)
-        mixer.music.play()
+    def playMp3(self, filename):
+        player = pyglet.media.Player()
+        music = pyglet.resource.media(filename)
+        player.queue(music)
+        player.play()
+    
+        while True:
+            time.sleep(0.1)        
+            if player.time == 0:
+                break;
 
     def make_answer(self, statement):
         return self.bot.get_response(statement)
@@ -81,6 +89,9 @@ class Speech_AI:
         def clean_up():
             os.remove(self._mp3_name)
 
+
+def stop():
+    print('stop')
 
 def main():
     ai = Speech_AI()
